cd3b4c985c2ff2fdad0d64c9868c13e8dc3fecd1
sp0ng3808 <tediar1337@gmail.com>
2024-12-15 03:51:55 +0700

    Enhance AdminLogin component with navigation link to Log In page
    - Added a link for users who already have an account to navigate to the Log In page.
    - Imported Link from react-router-dom to facilitate navigation.
    
    This change improves user experience by providing a clear path for existing users to access their accounts.

--- frontend/admin/src/components/AdminLogin/index.jsx
+++ frontend/admin/src/components/AdminLogin/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import "./style.css";
 import axiosInstance from "../../axiosInstance";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -64,6 +64,14 @@ const LoginForm = () => {
             Login
           </button>
         </form>
+        <div className="">
+          <p>
+            Already have an account?{" "}
+            <Link to="/Login" className="text-blue-500">
+              Log In
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
